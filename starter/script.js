8241a09aebba39c35313cb38007429fd3bb53150
Avinash sharma <sharmaavi2196@gmail.com>
2025-01-29 23:13:58 +0530

    Refactor displayMovements function to use arrow functions and improve HTML structure; update CSS font-family quotes

--- starter/script.js
+++ starter/script.js
@@ -61,6 +61,21 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = movements => {
+  containerMovements.innerHTML = '';
+  movements.forEach((mov, i, arr) => {
+    const type = mov > 0 ? `deposit` : `withdrawal`;
+
+    const html = ` <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+  
+    <div class="movements__value">${mov}</div>
+    </div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -91,26 +106,6 @@ movements.forEach((mov, i, arr) => {
   }
 });
 /////////////////////////////////////////////////
-const displayMovements = function (movements, sort = false) {
-  containerMovements.innerHTML = '';
-
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
-
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
-
-    const html = `
-      <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
-        <div class="movements__value">${mov}€</div>
-      </div>
-    `;
-
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  });
-};
 
 //SLICE
 let arr = ['a', 'b', 'c', 'd', 'e'];
